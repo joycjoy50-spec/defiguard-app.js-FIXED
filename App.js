928fbd9facbf18c56de88805d2bbaaedae050c25
ADU <joycjoy50@gmail.com>
2026-05-23 01:45:27 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,96 @@
+import React, { useEffect, useState } from 'react';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import './App.css';
+import './assets/css/responsive.css';
+import './assets/css/style.css';
+
+import Home from "./pages/home";
+import Services from './pages/services';
+import About from './pages/about';
+import Features from './pages/features';
+import Team from './pages/team';
+import Blog from './pages/blog';
+import Contact from './pages/contact';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  }, {
+    path: "/services",
+    element: <Services />,
+  }, {
+    path: "/about",
+    element: <About />,
+  }, {
+    path: "/features",
+    element: <Features />,
+  }, {
+    path: "/team",
+    element: <Team />,
+  }, {
+    path: "/team",
+    element: <Team />,
+  }, {
+    path: "/blog",
+    element: <Blog />,
+  }, {
+    path: "/contact",
+    element: <Contact />,
+  },
+]);
+
+function App() {
+
+  const [isBackendConnected, setIsBackendConnected] = useState(true);
+
+  useEffect(() => {
+    const checkBackendConnection = async () => {
+      try {
+        const response = await fetch('http://localhost:3003/ping');
+        if (!response.ok) {
+          setIsBackendConnected(false);
+          console.log('here')
+        }
+      } catch (error) {
+        setIsBackendConnected(false);
+      }
+    };
+    checkBackendConnection();
+  }, []);
+
+  return (
+    <div>
+      {isBackendConnected ? (
+        <RouterProvider 
+          router={router} 
+        />
+      ) : (
+        <div>Can't access to Backend API</div>
+      )}
+    </div>
+  );
+}
+
+// <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+
+export default App;
